0eab07de7f217e46d15abc3307f575d7978b896c
Nick Podratz <nick.podratz@syntax-institut.de>
2026-06-23 12:02:05 +0200

    Migrate to ts and add /login

---
+++ app.ts
@@ -0,0 +1,87 @@
+// src/server.ts
+import express from 'express';
+import { randomUUID } from 'node:crypto';
+
+const port = 3000;
+const app = express();
+
+let count = 0;
+let todos: string[] = [];
+
+app.get('/', (_, res) => {
+  res.send('Hello, Syntax!');
+});
+
+app.get('/counter', (_, res) => {
+    res.json({ count });
+});
+
+app.post('/counter', (_, res) => {
+    count += 1;
+    res.status(204).send();
+});
+
+app.get("/health", (_, res) => {
+    res.json({
+        message: "Server is running",
+        timestamp: new Date().toISOString()
+    })
+})
+
+app.post("/todos", (req, res) => {
+    const { text } = req.query as { text?: string };
+    if (text) {
+        todos.push(text);
+        res.status(201).send();
+    } else {
+        res.status(400)
+    }
+})
+
+app.get("/todos", (_, res) => {
+    res.status(200).json({ todos });
+})
+
+app.get("/todos/:id", (req, res) => {
+    const { id } = req.params  as { id?: string };
+    const index = Number(id);
+
+    if (index < todos.length) {
+        const todo = todos[index];
+        res.status(200).json({ text: todo });
+    } else {
+        res.status(404).send();
+    }
+})
+
+type SessionId = string;
+type Session = {
+    username: string,
+    createdAt: Date
+}
+const sessions = new Map<SessionId, Session>()
+
+app.post("/login", (req, res) => {
+    const { username, password } = req.query as { username?: string, password?: string };
+
+    if (username !== "admin" || password !== "123") {
+        return res.status(401).json({ error: "Authentication failed" })
+    }
+
+    const sessionId = randomUUID();
+    const session: Session = {
+        username,
+        createdAt: new Date()
+    };
+    sessions.set(sessionId, session);
+    res.cookie("sessionId", sessionId, {
+        httpOnly: true,
+        secure: false,
+        sameSite: "lax"
+    })
+    res.send()
+})
+
+app.listen(port, () => {
+  console.log(`Server listening on port: ${port}`);
+});
