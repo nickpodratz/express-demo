86a9060eda382903163fbbb66dd1ed4cbbf2ffb0
Nick Podratz <nick.podratz@syntax-institut.de>
2026-07-02 11:16:58 +0200

    Configure compose stack

--- db/pool.postgres.ts
+++ db/pool.postgres.ts
@@ -9,11 +9,11 @@ let pool: Pool | null = null; // Singelton
 export function getPostgresPool(): Pool {
   if (!pool) {
     pool = new Pool({
-      host: process.env.DB_HOST,
-      port: Number(process.env.DB_PORT),
-      user: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME,
+      host: process.env.POSTGRES_HOST,
+      port: Number(process.env.POSTGRES_PORT),
+      user: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DB,
 
       max: 10,
       idleTimeoutMillis: 30_000,
